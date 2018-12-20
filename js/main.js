0247e4cea9846f7d51ecb16589101a0e781ef99a
Mario Freire <huege.design@gmail.com>
2018-12-20 01:30:24 -0200

    feat: adds a function that hands out the cards

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 
 var icons = ["🥫", "🍲", "🥘", "🚙", "✈", "🍵", "🛵", "🥤"];
 
-let cards = $('.card');
-
 function randomCheck () {
     let rndm = Math.floor((Math.random() * 20));
-    if (rndm > icons.length) {
+    if (rndm > icons.length - 1) {
         return randomCheck();
     }    
     else {
@@ -13,8 +11,17 @@ function randomCheck () {
     }
 }
 
-console.log(randomCheck());
+function cardHandler () {
+    for (var i=0; i < $('.card').length; i++) {
+        $('.card').eq(i).text(icons[i]);
+    }
+    $('.card').map(function () {
+        $(this).clone().insertAfter($(this));
+    });
+    for (var j=0; i < $('.card').length; i++) {
+        let x = $('.card:odd').detach();
+        x.insertAfter($('.card').eq(randomCheck()));
+    }
+}
 
-// cards.map(function () {
-//     $(this).text(icons[i]);
-// });
+cardHandler();
