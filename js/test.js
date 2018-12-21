7578997ee1153688b4daae96adc55790d821a803
Mario Freire <huege.design@gmail.com>
2018-12-21 17:24:20 -0200

    test: wraps random array generator inside cardDealer function

--- js/test.js
+++ js/test.js
@@ -4,33 +4,35 @@ var icons = ["🥫", "🍲", "🥘", "🚙", "✈", "🍵",
     "♎", "♐", "♑", "♒", "♓", "⛎", "🙈", "🙉",
     "🙊", "🚀"];
 
-var biggerBox = [];
-
-function genRandom() {
-    while (biggerBox.length < icons.length) {
-        console.log("genRandom has started");
-        let rndm = Math.floor((Math.random() * 40));
-        console.log("number is " + rndm);
-        console.log("genRandom ended")
-        return checkAndPlaceNumber(rndm);
+function cardDealer () {
+    var biggerBox = [];
+    function genRandom() {
+        while (biggerBox.length < icons.length) {
+            console.log("genRandom has started");
+            let rndm = Math.floor((Math.random() * 40));
+            console.log("number is " + rndm);
+            console.log("genRandom ended")
+            return checkAndPlaceNumber(rndm);
+        };
     };
+
+    var checkAndPlaceNumber = function(number) {
+        console.log("checkAndPlaceNumber has started");
+        console.log(number + " is caught");
+        if ((biggerBox.includes(number)) || (number >= icons.length))  {
+            console.log("The number is not fit")
+            return genRandom();
+        }
+        else {
+            var box = number;
+            biggerBox.push(box);
+            console.log(biggerBox + " in biggerBox");
+            return genRandom();
+        };
+    };
+
+    genRandom();
+    console.log(biggerBox.length);
+    console.log(biggerBox);
 };
 
-var checkAndPlaceNumber = function(number) {
-    console.log("checkAndPlaceNumber has started");
-    console.log(number + " is caught");
-    if ((biggerBox.includes(number)) || (number >= icons.length))  {
-        console.log("The number is not fit")
-        return genRandom();
-    }
-    else {
-        var box = number;
-        biggerBox.push(box);
-        console.log(biggerBox + " in biggerBox");
-        return genRandom();
-    };
-};
-
-genRandom();
-console.log(biggerBox.length);
-console.log(biggerBox);
