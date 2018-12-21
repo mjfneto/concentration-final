437698684ca0a2ee2567e9fa7a0b8c437d264101
Mario Freire <huege.design@gmail.com>
2018-12-21 20:02:02 -0200

    chore: adds old function to archives

--- js/archives.js
+++ js/archives.js
@@ -11,3 +11,20 @@ function randomCheck() {
     };
 };
 //
+
+//
+function cardDealer() {
+    $('.grid').html($('<div>', { class: 'card' }));
+    let firstBorn = $('.card').eq(0);
+    for (var i = 0; i < ((icons.length / 2) - 1); i++) {
+        firstBorn.clone().insertAfter(firstBorn);
+    };
+    for (var j = 0; j < (icons.length / 2); j++) {
+        $('.card').eq(j).text(icons[j]);
+    };
+    $('.card').map(function () {
+        $(this).clone().insertAfter($(this));
+    });
+    let sliceDeck = $('.card').slice(randomShuffle()).detach();
+};
+//
