c40fc1d1816d51df5d1393c6830807a99cb2a920
Mario Freire <huege.design@gmail.com>
2019-01-03 20:44:18 -0200

    refactor: adjusts the start trigger to timer's start

--- js/main.js
+++ js/main.js
@@ -74,44 +74,60 @@ function flipCardEvaluate() {
             shown = [];
             $('.flip-card.rotate').css("visibility", "hidden").removeClass("rotate");
             if ($('.grid > div[style]').length >= 16) {
-                window.alert("Congratulations! You have found all the " + $('.grid > div[style]').length/2 + " pairs of cards.");
+                window.alert("Congratulations! You have found all the " + $('.grid > div[style]').length / 2 + " pairs of cards.");
             }
         }
     });
 };
 
-function handle_mousedown(e){
+function handle_mousedown(e) {
     window.my_dragging = {};
     my_dragging.pageX0 = e.pageX;
     my_dragging.elem = this;
     my_dragging.offset0 = $(this).offset();
-    function handle_dragging(e){
+    function handle_dragging(e) {
         var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
-        $(my_dragging.elem)
-        .offset({left: left});
-        if ($(my_dragging.elem).offset().left <= (95/100 * my_dragging.offset0.left)) {
-            $(my_dragging.elem).offset({left: my_dragging.offset0.left});
-            $('.menu').mousedown(handle_mousedown);
+        $(my_dragging.elem).offset({ left: left });
+        if (Math.floor(left) <= Math.floor(95 / 100 * my_dragging.offset0.left)) {
+            $(my_dragging.elem).offset({ left: my_dragging.offset0.left });
+            $('body')
+                .off('mousemove', handle_dragging)
+                .off('mouseup', handle_mouseup);
             startTimer();
             playGame();
         };
     }
     function handle_mouseup(e) {
         $('body')
-        .off('mousemove', handle_dragging)
-        .off('mouseup', handle_mouseup);
+            .off('mousemove', handle_dragging)
+            .off('mouseup', handle_mouseup);
     }
     $('body')
-    .on('mouseup', handle_mouseup)
-    .on('mousemove', handle_dragging);
+        .on('mouseup', handle_mouseup)
+        .on('mousemove', handle_dragging);
 }
 
 function startTimer() {
-    var date = new Date;
-    var timerDisplay = $('.timer').find("p");
-    setInterval(goesTheClock, 1000);
-    function goesTheClock() {
-        tickTock = Math.floor((new Date - date) / 1000);
-        timerDisplay.text(tickTock);
-    };
+    var seconds = 0;
+    var minutes = 0;
+    var timer = setInterval(function () {
+        seconds++;
+        if (seconds === 59) {
+            minutes++;
+            seconds = 0;
+        }
+        var secondsToString = checkNumber(seconds);
+        var minutesToString = checkNumber(minutes);
+        $('.timer > p').text(minutesToString + ":" + secondsToString)
+    }, 1000);
+};
+
+function checkNumber(nbr) {
+    if (nbr < 10) {
+        nbr = ("0" + nbr);
+        return nbr;
+    }
+    else {
+        return nbr;
+    }
 };
